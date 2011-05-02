494d77079eb343523162f502f3b84012e8e05509
Elia L <kkkotze@web.de>
2011-05-02 17:01:09 +0200

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kap3::Application.config.secret_token = '91cc62269f8c09948b264cc000586ce6c121e5335746b4afca62434429743dd29cdecf833b068dafeac775c3dfcc5900c427cbed9f8a38331304cb3b41451c82'
